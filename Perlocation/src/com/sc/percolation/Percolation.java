b1d6b43c60393ca52aa1691d2eea21593a882825
ueec6tv <extern_kuriakose.nikhil@allianz.com>
2014-06-25 16:47:39 +0530

    init commit for perlocation

---
+++ Perlocation/src/com/sc/percolation/Percolation.java
@@ -0,0 +1,22 @@
+package com.sc.percolation;
+
+public class Percolation {
+	   public Percolation(int N){
+		   // create N-by-N grid, with all sites blocked
+	   }
+	   public void open(int xOrdinate, int yOrdinate)  {
+		   // open site (row i, column j) if it is not already
+	   }
+	   public boolean isOpen(int xOrdinate, int yOrdinate) {
+		   // is site (row i, column j) open?
+		   return false;
+	   }
+	   public boolean isFull(int xOrdinate, int yOrdinate)  {
+		   // is site (row i, column j) full?
+		   return false;
+	   }
+	   public boolean percolates()       {
+		   // does the system percolate?
+		   return false;
+	   }
+	}
