7e15fa94f8c6e7e33e82954c3f164ed143dc533b
nikhilkuria <nikhilkuria@hotmail.com>
2014-07-08 12:42:03 +0530

    Subset.java

--- GenericQueue/src/com/sc/queue/Deque.java
+++ GenericQueue/src/com/sc/queue/Deque.java
@@ -3,161 +3,142 @@ package com.sc.queue;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Deque<Item> implements Iterable<Item>
-{
-    private Node startNode;
-    private Node endNode;
-    private int dequeLength;
+public class Deque<Item> implements Iterable<Item> {
+	private Node startNode;
+	private Node endNode;
+	private int dequeLength;
 
-    public Deque()
-    {
-    }
+	public Deque() {
+	}
 
-    public boolean isEmpty()
-    {
-        return dequeLength==0? true : false;
-    }
+	public boolean isEmpty() {
+		return dequeLength == 0 ? true : false;
+	}
 
-    public int size()
-    {
-        return dequeLength;
-    }
+	public int size() {
+		return dequeLength;
+	}
 
-    public void addFirst(Item item)
-    {
-        validateItem(null);
-        Node node = new Node<Item>(item);
-		if(startNode == null){
+	public void addFirst(Item item) {
+		validateItem(null);
+		Node node = new Node<Item>(item);
+		if (startNode == null) {
 			sanitizeDeque(node);
-		}else{
+		} else {
 			node.nextNode = startNode;
 			startNode.previousNode = node;
 			startNode = node;
 		}
 		dequeLength++;
-    }
+	}
 
+	public void addLast(Item item) {
+		validateItem(item);
+		Node node = new Node<>(item);
+		if (endNode == null) {
+			sanitizeDeque(node);
+		} else {
+			node.previousNode = endNode;
+			endNode.nextNode = node;
+			endNode = node;
+		}
+		dequeLength++;
+	}
 
-	public void addLast(Item item)
-    {
-	    validateItem(item);
-    	Node node = new Node<>(item);
-    	if(endNode == null){
-    		sanitizeDeque(node);
-    	}else{
-    		node.previousNode = endNode;
-    		endNode.nextNode = node;
-    		endNode = node;
-    	}
-    	dequeLength++;
-    }
+	public Item removeFirst() {
+		if (size() < 1) {
+			throw new NoSuchElementException();
+		}
+		// Should revisit this cast
+		Item itemToReturn = (Item) startNode.item;
+		startNode = startNode.nextNode;
+		startNode.previousNode = null;
+		dequeLength--;
+		return itemToReturn;
+	}
 
-    public Item removeFirst()
-    {
-    	if(size()<1){
-    		throw new NoSuchElementException();
-    	}
-        //Should revisit this cast
-        Item itemToReturn = (Item)startNode.item;
-        startNode = startNode.nextNode;
-        startNode.previousNode = null;
-        dequeLength--;
-        return itemToReturn;
-    }
+	public Item removeLast() {
+		if (size() < 1) {
+			throw new NoSuchElementException();
+		}
+		Item itemToRetuen = (Item) endNode.item;
+		endNode = endNode.previousNode;
+		endNode.nextNode = null;
+		dequeLength--;
+		return itemToRetuen;
+	}
 
-    public Item removeLast()
-    {
-    	if(size()<1){
-    		throw new NoSuchElementException();
-    	}
-        Item itemToRetuen = (Item)endNode.item;
-        endNode = endNode.previousNode;
-        endNode.nextNode = null;
-        dequeLength--;
-        return itemToRetuen;
-    }
+	public Iterator<Item> iterator() {
+		return new DequeIterator();
+	}
 
-    public Iterator<Item> iterator()
-    {
-        return new DequeIterator();
-    }
-
-    private void sanitizeDeque(Node node) {
-		if(startNode == null){
+	private void sanitizeDeque(Node node) {
+		if (startNode == null) {
 			startNode = node;
 			endNode = node;
 			startNode.nextNode = endNode;
 			endNode.previousNode = startNode;
 		}
-		
+
 	}
-    
-    private void validateItem(Item item)
-    {
-        if (item == null) {
-            throw new NoSuchElementException(
-                    "No place for foul creatures like you");
-        }
 
-    }
+	private void validateItem(Item item) {
+		if (item == null) {
+			throw new NoSuchElementException(
+					"No place for foul creatures like you");
+		}
 
-    public static void main(String[] args)
-    {
-        Deque<Integer> deque = new Deque<>();
-        deque.removeFirst();
-        deque.addFirst(1);
-        deque.addFirst(2);
-        deque.addLast(99);
-        deque.removeFirst();
-        deque.removeLast();
-        for (Integer val : deque) {
+	}
+
+	public static void main(String[] args) {
+		Deque<Integer> deque = new Deque<>();
+		deque.addFirst(1);
+		deque.addFirst(2);
+		deque.addLast(99);
+		deque.removeFirst();
+		deque.removeLast();
+		for (Integer val : deque) {
 			System.out.println(val);
 		}
-        
-    }
-    
-    private class Node<Item>
-    {
-    	Node(){
-    		
-    	}
-    	
-        Node(Item item){
-            this.item = item;
-        }
-        Node previousNode;
-        Node nextNode;
-        Item item;
-    }
-    
-    private class DequeIterator implements Iterator<Item>{
 
-    	Node header = startNode;
-    	
-        @Override
-        public boolean hasNext()
-        {
-            return header != null;
-        }
+	}
 
-        @Override
-        public Item next()
-        {
-            if(hasNext()){
-            	Item item = (Item) header.item;
-            	header = header.nextNode;
-                return item;
-            }else{
-                throw new NoSuchElementException();
-            }
-        }
+	private class Node<Item> {
+		Node() {
 
-        @Override
-        public void remove()
-        {
-            throw new UnsupportedOperationException();
-        }
-        
-    }
-    
+		}
+
+		Node(Item item) {
+			this.item = item;
+		}
+
+		Node previousNode;
+		Node nextNode;
+		Item item;
+	}
+
+	private class DequeIterator implements Iterator<Item> {
+
+		Node header = startNode;
+
+		public boolean hasNext() {
+			return header != null;
+		}
+
+		public Item next() {
+			if (hasNext()) {
+				Item item = (Item) header.item;
+				header = header.nextNode;
+				return item;
+			} else {
+				throw new NoSuchElementException();
+			}
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+	}
+
 }
