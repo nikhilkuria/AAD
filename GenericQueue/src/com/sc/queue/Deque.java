2b6c788cce154d134c473c9805776c07b8421598
Nikhil Kuriakose <nikhilkuria@hotmail.com>
2014-07-09 22:00:58 +0530

    not satisified with low performance

--- GenericQueue/src/com/sc/queue/Deque.java
+++ GenericQueue/src/com/sc/queue/Deque.java
@@ -3,156 +3,178 @@ package com.sc.queue;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Deque<Item> implements Iterable<Item> {
-	private Node startNode;
-	private Node endNode;
-	private int dequeLength;
+public class Deque<Item> implements Iterable<Item>
+{
+    private Node startNode;
+    private Node endNode;
+    private int dequeLength;
 
-	public Deque() {
-	}
+    public Deque()
+    {
+    }
 
-	public boolean isEmpty() {
-		return dequeLength == 0 ? true : false;
-	}
+    public boolean isEmpty()
+    {
+        return dequeLength == 0 ? true : false;
+    }
 
-	public int size() {
-		return dequeLength;
-	}
+    public int size()
+    {
+        return dequeLength;
+    }
 
-	public void addFirst(Item item) {
-		validateItem(item);
-		Node node = new Node<Item>(item);
-		if(startNode != null){
-			node.nextNode = startNode;
-			startNode.previousNode = node;
-		}
-		if(endNode == null){
-			endNode = node;
-		}
-		startNode = node;
-		dequeLength++;
-	}
+    public void addFirst(Item item)
+    {
+        validateItem(item);
+        Node node = new Node<Item>(item);
+        if (startNode != null) {
+            node.nextNode = startNode;
+            startNode.previousNode = node;
+        }
+        if (endNode == null) {
+            endNode = node;
+        }
+        startNode = node;
+        dequeLength++;
+    }
 
-	public void addLast(Item item) {
-		validateItem(item);
-		Node node = new Node<>(item);
-		if(endNode!=null){
-			endNode.nextNode=node;
-			node.previousNode=endNode;
-		}
-		if(startNode == null){
-			startNode = node;
-		}
-		endNode = node;
-		dequeLength++;
-	}
+    public void addLast(Item item)
+    {
+        validateItem(item);
+        Node node = new Node<>(item);
+        if (endNode != null) {
+            endNode.nextNode = node;
+            node.previousNode = endNode;
+        }
+        if (startNode == null) {
+            startNode = node;
+        }
+        endNode = node;
+        dequeLength++;
+    }
 
-	public Item removeFirst() {
-		if (size() < 1) {
-			throw new NoSuchElementException();
-		}
-		// Should revisit this cast
-		Item itemToReturn = (Item) startNode.item;
-		if(startNode.nextNode!=null){
-			startNode = startNode.nextNode;
-			startNode.previousNode = null;
-		}else{
-			startNode = null;
-			endNode = null;
-		}
-		dequeLength--;
-		return itemToReturn;
-	}
+    public Item removeFirst()
+    {
+        if (size() < 1) {
+            throw new NoSuchElementException();
+        }
+        // Should revisit this cast
+        Item itemToReturn = (Item) startNode.item;
+        if (startNode.nextNode != null) {
+            startNode = startNode.nextNode;
+            startNode.previousNode = null;
+        }
+        else {
+            startNode = null;
+            endNode = null;
+        }
+        dequeLength--;
+        return itemToReturn;
+    }
 
-	public Item removeLast() {
-		if (size() < 1) {
-			throw new NoSuchElementException();
-		}
-		Item itemToRetuen = (Item) endNode.item;
-		if(endNode.previousNode!=null){
-			endNode = endNode.previousNode;
-			endNode.nextNode = null;
-		}else{
-			startNode = null;
-			endNode = null;
-		}
+    public Item removeLast()
+    {
+        if (size() < 1) {
+            throw new NoSuchElementException();
+        }
+        Item itemToRetuen = (Item) endNode.item;
+        if (endNode.previousNode != null) {
+            endNode = endNode.previousNode;
+            endNode.nextNode = null;
+        }
+        else {
+            startNode = null;
+            endNode = null;
+        }
 
-		dequeLength--;
-		return itemToRetuen;
-	}
+        dequeLength--;
+        return itemToRetuen;
+    }
 
-	public Iterator<Item> iterator() {
-		return new DequeIterator();
-	}
+    public Iterator<Item> iterator()
+    {
+        return new DequeIterator();
+    }
 
-	private void sanitizeDeque(Node node) {
-		if (startNode == null) {
-			startNode = node;
-			endNode = node;
-		}
+    private void sanitizeDeque(Node node)
+    {
+        if (startNode == null) {
+            startNode = node;
+            endNode = node;
+        }
 
-	}
+    }
 
-	private void validateItem(Item item) {
-		if (item == null) {
-			throw new NullPointerException(
-					"No place for foul creatures like you");
-		}
+    private void validateItem(Item item)
+    {
+        if (item == null) {
+            throw new NullPointerException(
+                    "No place for foul creatures like you");
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
-		Deque<Integer> deque = new Deque<>();
-		deque.addFirst(1);
-		deque.addLast(2);
-		deque.addLast(3);
-		deque.addFirst(0);
-		deque.removeLast();
-		deque.removeFirst();
-		deque.removeFirst();
-		deque.removeFirst();
-		for (Integer val : deque) {
-			System.out.println(val);
-		}
+    public static void main(String[] args)
+    {
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(1);
+        deque.addLast(2);
+        deque.addLast(3);
+        deque.addFirst(0);
+        deque.removeLast();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        for (Integer val : deque) {
+            System.out.println(val);
+        }
 
-	}
+    }
 
-	private class Node<Item> {
-		Node() {
+    private class Node<Item>
+    {
+        Node()
+        {
 
-		}
+        }
 
-		Node(Item item) {
-			this.item = item;
-		}
+        Node(Item item)
+        {
+            this.item = item;
+        }
 
-		Node previousNode;
-		Node nextNode;
-		Item item;
-	}
+        Node previousNode;
+        Node nextNode;
+        Item item;
+    }
 
-	private class DequeIterator implements Iterator<Item> {
+    private class DequeIterator implements Iterator<Item>
+    {
 
-		Node header = startNode;
+        Node header = startNode;
 
-		public boolean hasNext() {
-			return header != null;
-		}
+        public boolean hasNext()
+        {
+            return header != null;
+        }
 
-		public Item next() {
-			if (hasNext()) {
-				Item item = (Item) header.item;
-				header = header.nextNode;
-				return item;
-			} else {
-				throw new NoSuchElementException();
-			}
-		}
+        public Item next()
+        {
+            if (hasNext()) {
+                Item item = (Item) header.item;
+                header = header.nextNode;
+                return item;
+            }
+            else {
+                throw new NoSuchElementException();
+            }
+        }
 
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
 
-	}
+    }
 
 }
--- GenericQueue/src/com/sc/queue/RandomizedQueue.java
+++ GenericQueue/src/com/sc/queue/RandomizedQueue.java
@@ -4,161 +4,214 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import com.princeton.stdlib.StdRandom;
+import com.princeton.stdlib.Stopwatch;
 
-public class RandomizedQueue<Item> implements Iterable<Item> {
+public class RandomizedQueue<Item> implements Iterable<Item>
+{
 
-	private final int ARRAY_SIZE_FACTOR = 20;
+    private final int ARRAY_SIZE_FACTOR = 20;
 
-	private Item[] queue;
-	private int size;
+    private Item[] queue;
+    private int size;
 
-	public RandomizedQueue() {
-		queue = (Item[]) new Object[ARRAY_SIZE_FACTOR];
-	}
+    public RandomizedQueue()
+    {
+        queue = (Item[]) new Object[ARRAY_SIZE_FACTOR];
+    }
 
-	public boolean isEmpty() {
-		return size == 0 ? true : false;
+    public boolean isEmpty()
+    {
+        return size == 0 ? true : false;
 
-	}
+    }
 
-	public int size() {
-		return size;
+    public int size()
+    {
+        return size;
 
-	}
+    }
 
-	public void enqueue(Item item) {
-		validateItem(item);
-		if (size == queue.length) {
-			resize(queue.length * 2);
+    public void enqueue(Item item)
+    {
+        validateItem(item);
+        if (size == queue.length) {
+            resize(queue.length * 2);
+        }
+        queue[size] = item;
+        size++;
+    }
+
+    public Item dequeue()
+    {
+        if (size() < 1) {
+            throw new NoSuchElementException();
+        }
+        if (size < queue.length / 4) {
+            resize(queue.length / 2);
+        }
+        int randomIndex = getRandomNumber();
+        size--;
+        Item randomItem = queue[randomIndex];
+        removeItem(randomIndex);
+        // System.out.println("Removing element from index :"+randomIndex);
+
+        return randomItem;
+
+    }
+
+    public Item sample()
+    {
+        if (size() < 1) {
+            throw new NoSuchElementException();
+        }
+        int randomIndex = getRandomNumber();
+        Item randomItem = queue[randomIndex];
+        return randomItem;
+
+    }
+
+    public Iterator<Item> iterator()
+    {
+        return new RandomQueueIterator();
+
+    }
+
+    public static void main(String[] args)
+    {
+        RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        Stopwatch watch = new Stopwatch();
+		for (int i = 1; i < 1280000; i++) {
+		    //System.out.println(i);
+		    /*if(i%7==0){
+		        //System.out.println("Deque");
+		        queue.dequeue();
+		    }else if(i%13==0){
+	              //System.out.println("Sample");
+		        queue.sample();
+		    }else if(i%9==0){
+	              //System.out.println("Empty");
+		        queue.isEmpty();
+		    }else if(i%15==0){
+		        queue.size();
+		    }
+		    else{
+		          //System.out.println("Enque");
+		          queue.enqueue(i);
+		    }*/
+		    queue.enqueue(i);
 		}
-		queue[size] = item;
-		size++;
-	}
+		System.out.println(watch.elapsedTime());
+	      for (int i = 1; i < 1280000; i++) {
+	            //System.out.println(i);
+	            /*if(i%7==0){
+	                //System.out.println("Deque");
+	                queue.dequeue();
+	            }else if(i%13==0){
+	                  //System.out.println("Sample");
+	                queue.sample();
+	            }else if(i%9==0){
+	                  //System.out.println("Empty");
+	                queue.isEmpty();
+	            }else if(i%15==0){
+	                queue.size();
+	            }
+	            else{
+	                  //System.out.println("Enque");
+	                  queue.enqueue(i);
+	            }*/
+	            queue.dequeue();
+	        }
+		System.out.println(watch.elapsedTime());
+    }
 
-	public Item dequeue() {
-		if (size() < 1) {
-			throw new NoSuchElementException();
-		}
-		if (size < queue.length / 4) {
-			resize(queue.length / 2);
-		}
-		int randomIndex = getRandomNumber();
-		size--;
-		Item randomItem = queue[randomIndex];
-		removeItem(randomIndex);
-		// System.out.println("Removing element from index :"+randomIndex);
+    private void resize(int length)
+    {
+        // System.out.println("Resizing Array : "+queue.length+" to "+length);
+        Item[] tempQueue = (Item[]) new Object[length];
+        for (int index = 0; index < size(); index++) {
+            tempQueue[index] = queue[index];
+        }
+        queue = tempQueue;
+    }
 
-		return randomItem;
+    private int getRandomNumber()
+    {
+        return StdRandom.uniform(0, size());
+    }
 
-	}
+    private void removeItem(int removeIndex)
+    {
+       for (int index = removeIndex; index < size(); index++) {
+            Item newValue = queue[index + 1];
+            if (newValue == null) {
+                return;
+            }
+            queue[index] = newValue;
+        }
+        queue[size()] = null;
+    }
 
-	public Item sample() {
-		if (size() < 1) {
-			throw new NoSuchElementException();
-		}
-		int randomIndex = getRandomNumber();
-		Item randomItem = queue[randomIndex];
-		return randomItem;
+    private void validateItem(Item item)
+    {
+        if (item == null) {
+            throw new NullPointerException(
+                    "No place for foul creatures like you");
+        }
 
-	}
+    }
 
-	public Iterator<Item> iterator() {
-		return new RandomQueueIterator();
+    private class RandomQueueIterator implements Iterator<Item>
+    {
 
-	}
+        int size;
+        private int[] usedIndices;
 
-	public static void main(String[] args) {
-		RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        public RandomQueueIterator()
+        {
+            size = size();
+            usedIndices = new int[size];
+            for (int i = 0; i < size; i++) {
+                usedIndices[i] = -1;
+            }
+        }
 
-/*		queue.enqueue(1);
-		queue.enqueue(2);
-		queue.enqueue(3);
-		queue.enqueue(4);
-		queue.enqueue(5);
-		queue.enqueue(6);*/
-/*		for (int i = 0; i < 100000; i++) {
-			queue.enqueue(i);
-		}*/
+        public boolean hasNext()
+        {
+            return size == 0 ? false : true;
+        }
 
-		//System.out.println(queue.dequeue());
-		//System.out.println(queue.dequeue());
+        public Item next()
+        {
+            if(hasNext()){
+                int index = getRandomNumber();
+                Item item = queue[index];
+                if (item == null) {
+                    throw new NoSuchElementException();
+                }
+                usedIndices[index] = index;
+                size--;
+                return item;
+            }        else {
+                throw new NoSuchElementException();
+            }
+           
+            
+        }
 
-		for (Integer integer : queue) {
-			System.out.println(integer);
-		}
-	}
+        private int getRandomNumber()
+        {
+            int randomIndex = StdRandom.uniform(0, usedIndices.length);
+            while (usedIndices[randomIndex] == randomIndex) {
+                randomIndex = StdRandom.uniform(0, usedIndices.length);
+            }
+            return randomIndex;
+        }
 
-	private void resize(int length) {
-		// System.out.println("Resizing Array : "+queue.length+" to "+length);
-		Item[] tempQueue = (Item[]) new Object[length];
-		for (int index = 0; index < size(); index++) {
-			tempQueue[index] = queue[index];
-		}
-		queue = tempQueue;
-	}
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
 
-	private int getRandomNumber() {
-		return StdRandom.uniform(0, size());
-	}
+        }
 
-	private void removeItem(int removeIndex) {
-		for (int index = removeIndex; index < size(); index++) {
-			Item newValue = queue[index + 1];
-			if (newValue == null) {
-				return;
-			}
-			queue[index] = newValue;
-		}
-		queue[size()] = null;
-	}
-
-	private void validateItem(Item item) {
-		if (item == null) {
-			throw new NullPointerException(
-					"No place for foul creatures like you");
-		}
-
-	}
-
-	private class RandomQueueIterator implements Iterator<Item> {
-
-		int size;
-		private int[] usedIndices;
-
-		public RandomQueueIterator(){
-			size = size();
-			usedIndices = new int[size];
-			for (int i = 0; i < size; i++) {
-				usedIndices[i] = -1;
-			}
-		}
-		
-		public boolean hasNext() {
-			return size == 0 ? false : true;
-		}
-
-		public Item next() {
-			int index = getRandomNumber();
-			Item item = queue[index];
-			if (item == null) {
-				throw new NoSuchElementException();
-			}
-			usedIndices[index] = index;
-			return item;
-		}
-
-		private int getRandomNumber() {
-			int randomIndex = StdRandom.uniform(0, size);
-			while(usedIndices[randomIndex] == randomIndex){
-				randomIndex = StdRandom.uniform(0, size);
-			}
-			return randomIndex;
-		}
-
-		public void remove() {
-			throw new UnsupportedOperationException();
-
-		}
-
-	}
+    }
 }
