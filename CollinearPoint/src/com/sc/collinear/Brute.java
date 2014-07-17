ec0174107a22298ef2a2c57128fd2772a782ab53
nikhilkuria <nikhilkuria@hotmail.com>
2014-07-17 18:18:53 +0530

    Working build of brute and elegant

--- CollinearPoint/src/com/sc/collinear/Brute.java
+++ CollinearPoint/src/com/sc/collinear/Brute.java
@@ -1,79 +1,72 @@
 package com.sc.collinear;
 
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import com.princeton.stdlib.In;
 import com.princeton.stdlib.StdDraw;
+import com.sc.sort.elementary.Insertion;
 
 public class Brute {
+
+	private static final String POINT_SEPARATOR = " -> ";
 
 	public static void main(String args[]) {
 		initilizeCanvas();
 		String filename = args[0];
-		List<Point> pointList = generatePointList(filename);
-		List<Set<Point>> collinearPoints = findCollinearPoints(pointList);
-		StdDraw.setPenRadius(.003);
-		StdDraw.setPenColor(Color.BLACK);
-		for (Set<Point> line : collinearPoints) {
-			Point originpoint = null;
-			for (Point point : line) {
-				if (originpoint == null) {
-					originpoint = point;
-				} else {
-					originpoint.drawTo(point);
-					System.out.print(" -> ");
-				}
-				System.out.print(point.toString());
-			}
-			System.out.println();
-		}
+		Point[] points = generatePointList(filename);
+		findCollinearPoints(points);
+		
 	}
 
-	private static List<Set<Point>> findCollinearPoints(List<Point> pointList) {
-		List<Set<Point>> collinearPointsList = new ArrayList<>();
-		Point[] pointArray = pointList.toArray(new Point[pointList.size()]);
-		for (int firstIndex = 0; firstIndex < pointArray.length; firstIndex++) {
-			Point firstPoint = pointArray[firstIndex];
-			// collinearSet.add(firstPoint);
-			for (int secondIndex = firstIndex + 1; secondIndex < pointArray.length; secondIndex++) {
-				Set<Point> collinearPoints = new HashSet<>();
-				Point secondPoint = pointArray[secondIndex];
-				Double slope = firstPoint.slopeTo(secondPoint);
-				collinearPoints.add(secondPoint);
-				collinearPoints.add(firstPoint);
-				for (int thirdIndex = secondIndex + 1; thirdIndex < pointArray.length; thirdIndex++) {
-					Point thirdPoint = pointArray[thirdIndex];
-					if (firstPoint.slopeTo(thirdPoint) == slope) {
-						collinearPoints.add(thirdPoint);
-						for (int fourthIndex = thirdIndex + 1; fourthIndex < pointArray.length; fourthIndex++) {
-							Point fourthPoint = pointArray[fourthIndex];
-							if (firstPoint.slopeTo(fourthPoint) == slope) {
-								collinearPoints.add(fourthPoint);
-								collinearPointsList.add(collinearPoints);
-								continue;
+	private static void findCollinearPoints(Point[] points) {
+		Insertion.sort(points);
+		for (int firstIndex = 0; firstIndex < points.length; firstIndex++) {
+			Point[] outputString = new Point[4];
+			Point firstPoint = points[firstIndex];
+			outputString[0] = firstPoint;
+			for (int secondIndex = firstIndex+1; secondIndex < points.length; secondIndex++) {
+				Point secondPoint = points[secondIndex];
+				Double lineSlope = firstPoint.slopeTo(secondPoint);
+				outputString[1] = secondPoint;
+				for (int thirdIndex = secondIndex+1; thirdIndex < points.length; thirdIndex++) {
+					Point thirdPoint = points[thirdIndex];
+					if(firstPoint.slopeTo(thirdPoint)==lineSlope){
+						outputString[2] = thirdPoint;
+						for (int fourthIndex = thirdIndex+1; fourthIndex < points.length; fourthIndex++) {
+							Point fourthPoint = points[fourthIndex];
+							if(firstPoint.slopeTo(fourthPoint)==lineSlope){
+								outputString[3] = fourthPoint;
+								printLine(outputString);
 							}
 						}
 					}
-
 				}
 			}
 		}
-		return collinearPointsList;
+		
 	}
 
-	private static List<Point> generatePointList(String filename) {
+	private static void printLine(Point[] outputString) {
+		Insertion.sort(outputString);
+		int index = 1;
+		System.out.print(outputString[0].toString());
+		do{
+			System.out.print(POINT_SEPARATOR);
+			Point point = outputString[index];
+			System.out.print(point.toString());
+			index++;
+		}while(index<outputString.length);
+		outputString[0].drawTo(outputString[3]);
+		System.out.println();
+	}
+
+	private static Point[] generatePointList(String filename) {
 		In in = new In(filename);
-		int N = in.readInt();
-		List<Point> points = new ArrayList<Point>(N);
-		for (int i = 0; i < N; i++) {
+		int size = in.readInt();
+		Point[] points = new Point[size];
+		for (int i = 0; i < size; i++) {
 			int x = in.readInt();
 			int y = in.readInt();
 			Point point = new Point(x, y);
-			points.add(point);
+			points[i] = point;
 			point.draw();
 		}
 		return points;
@@ -82,8 +75,8 @@ public class Brute {
 	private static void initilizeCanvas() {
 		StdDraw.clear();
 		StdDraw.setPenColor(StdDraw.RED);
-		StdDraw.setXscale(0, 20000);
-		StdDraw.setYscale(0, 20000);
+		StdDraw.setXscale(0, 40);
+		StdDraw.setYscale(0, 40);
 		StdDraw.setPenRadius(.008);
 	}
 
