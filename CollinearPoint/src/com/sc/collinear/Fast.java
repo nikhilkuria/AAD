2f51ea73e07833178a4fea7b290119bb823230d1
nikhilkuria <nikhilkuria@hotmail.com>
2014-07-18 10:02:37 +0530

    adding sort on slope

--- CollinearPoint/src/com/sc/collinear/Fast.java
+++ CollinearPoint/src/com/sc/collinear/Fast.java
@@ -1,5 +1,8 @@
 package com.sc.collinear;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import com.princeton.stdlib.In;
 import com.princeton.stdlib.StdDraw;
 
@@ -15,15 +18,25 @@ public class Fast {
 	private static void findCollinearPoints(Point[] points) {
 		for (int originIndex = 0; originIndex < points.length; originIndex++) {
 			Point originPoint = points[originIndex];
-			Double[] slopes = new Double[points.length];
-			for (int index = 0; index < points.length; index++) {
-				if(originIndex!=index){
-					Point point = points[index];
-					double slope = originPoint.slopeTo(point);
-					slopes[index] = slope;
-				}
-			}
+			SlopeComparator slopeComparator = new SlopeComparator(originPoint);
+			Arrays.sort(points, slopeComparator);
+			findLinesFromOrigin(originPoint,points);
+			//printSlopes(points, originPoint);
 		}
+	}
+
+	private static void findLinesFromOrigin(Point originPoint, Point[] points) {
+		double[] slopes = new double[points.length];
+		for (int index = 0; index < points.length; index++) {
+			slopes[index] = originPoint.slopeTo(points[index]);
+		}
+	}
+
+	private static void printSlopes(Point[] points, Point originPoint) {
+		for (Point point : points) {
+			System.out.print(originPoint.slopeTo(point)+" ");
+		}
+		System.out.println();
 	}
 
 	private static Point[] generatePointList(String filename) {
@@ -46,5 +59,21 @@ public class Fast {
 		StdDraw.setXscale(0, 40);
 		StdDraw.setYscale(0, 40);
 		StdDraw.setPenRadius(.008);
+	}
+	
+	static class SlopeComparator implements Comparator<Point>{
+		private Point originPoint;
+		public SlopeComparator(Point originPoint){
+			this.originPoint = originPoint;
+		}
+		@Override
+		public int compare(Point pointOne, Point pointTwo) {
+			double slopeOne = originPoint.slopeTo(pointOne);
+			double slopeTwo = originPoint.slopeTo(pointTwo);
+			if(slopeOne == slopeTwo){
+				return 0;
+			}
+			return slopeOne>slopeTwo ? 1 : -1; 
+		}
 	}
 }
