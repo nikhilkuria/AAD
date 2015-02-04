b4cebb590120316d7ba836436c290d1a687229f3
nikhilkuria <nikhilkuria@hotmail.com>
2015-02-04 15:04:58 +0530

    finishing percolation again

--- Perlocation/src/com/sc/percolation/again/Percolation.java
+++ Perlocation/src/com/sc/percolation/again/Percolation.java
@@ -4,52 +4,109 @@ import com.princeton.stdlib.StdIn;
 
 public class Percolation {
 	
+	private final int INVALID_NODE = -1;
+	
 	private int[] grid;
 	private int[] componentHeight;
+	private int[] openNodes;
 	private int initialNode;
 	private int finalNode;
+	private int dimension;
 	
 	public Percolation(int gridDimension) {
 		int totalGridSize = gridDimension*gridDimension;
 		grid = new int[totalGridSize+2];
+		dimension = gridDimension;
 		componentHeight = new int[totalGridSize+2];
+		openNodes = new int[totalGridSize+2];
 		initialNode = totalGridSize;
 		finalNode = totalGridSize+1;
 		for (int index = 0; index < totalGridSize+2; index++) {
 			componentHeight[index] = 0;
 			grid[index] = index;
+			openNodes[index] = 0;
 		}
-		performInitialLinks(gridDimension);
+		openNodes[initialNode] = 1;
+		openNodes[finalNode] = 1;
+		//performInitialLinks(gridDimension);
 	}
-
-	private void performInitialLinks(int gridDimension) {
-		//Link Initial Node to top row
-		for (int topRowNode = 0; topRowNode < gridDimension; topRowNode++) {
-			connect(topRowNode, initialNode);
+	
+	public void open(int xCordinate, int yCordinate) {
+		validateArguments(xCordinate,yCordinate);
+		if(isOpen(xCordinate, yCordinate)){
+			return;
 		}
-		//Link bottom row to final Node
-		for (int bottomRowNode = (gridDimension*(gridDimension-1)); bottomRowNode < gridDimension*gridDimension; bottomRowNode++) {
-			connect(bottomRowNode, finalNode);
+		int cellIndex = getIndex(xCordinate, yCordinate);
+		//open to bottom, up, left and right cells
+		int upIndex = getIndex(xCordinate, yCordinate-1);
+		int downIndex = getIndex(xCordinate, yCordinate+1);
+		int leftIndex = getIndex(xCordinate-1, yCordinate);
+		int rightIndex = getIndex(xCordinate+1, yCordinate);
+		
+		openNodes[cellIndex] = 1;
+		
+		if(upIndex!=INVALID_NODE){
+			connect(cellIndex, upIndex);
 		}
-	}
-
-	public void open(int i, int j) {
+		if(downIndex!=INVALID_NODE){
+			connect(cellIndex, downIndex);
+		}
+		if(leftIndex!=INVALID_NODE){
+			connect(cellIndex, leftIndex);
+		}
+		if(rightIndex!=INVALID_NODE){
+			connect(cellIndex, rightIndex);
+		}
 		
 	}
 
-	public boolean isOpen(int i, int j) {
-		return true;
+	private void validateArguments(int xCordinate, int yCordinate) {
+		xCordinate--;
+		yCordinate--;
+		if((yCordinate<0) || yCordinate>(dimension-1)){
+			throw new IndexOutOfBoundsException();
+		}
+		if(xCordinate<0){
+			throw new IndexOutOfBoundsException();
+		}
+		if(xCordinate>(dimension-1)){
+			throw new IndexOutOfBoundsException();
+		}
 	}
 
-	public boolean isFull(int i, int j) {
-		return true;	
+	public boolean isOpen(int xCordinate, int yCordinate) {
+		validateArguments(xCordinate,yCordinate);
+		return openNodes[getIndex(xCordinate, yCordinate)]==1?true : false;
+	}
+
+	public boolean isFull(int xCordinate, int yCordinate) {
+		validateArguments(xCordinate,yCordinate);
+		return connected(initialNode, getIndex(xCordinate, yCordinate));	
 	}
 
 	public boolean percolates() {
-		return true;
+		return connected(initialNode, finalNode);// && (openNodes[initialNode]==1) && (openNodes[finalNode]==1);
+	}
+	
+	private int getIndex(int xCordinate, int yCordinate) {
+		xCordinate--;
+		yCordinate--;
+		if((yCordinate<0) || yCordinate>(dimension-1)){
+			return INVALID_NODE;
+		}
+		if(xCordinate<0){
+			return initialNode;
+		}
+		if(xCordinate>(dimension-1)){
+			return finalNode;
+		}
+		return (yCordinate*dimension) + xCordinate;
 	}
 	
 	private void connect(int sourceNode, int destinationNode){
+		if((openNodes[sourceNode]!=1)||(openNodes[destinationNode]!=1)){
+			return;
+		}
 		int sourceComponentRoot = getRoot(sourceNode);
 		int destinationComponentRoot = getRoot(destinationNode);
 		if(sourceComponentRoot==destinationComponentRoot){
@@ -81,22 +138,29 @@ public class Percolation {
 		return getRoot(origin)==getRoot(destination);
 	}
 	
+
 	private void printGrid() {
-		for (int element : grid) {
-			System.out.print(element+" ");
+		for (int index = 0; index < openNodes.length-2; index++) {
+			String status = openNodes[index] == 1? "O" : "X";
+			System.out.print(status+" ");
+			if((index+1)%dimension==0){
+				System.out.println();
+			}
 		}
-		System.out.println();
+		
 	}
 	
 	public static void main(String[] args) {
         int gridSize = StdIn.readInt();
         Percolation percolation = new Percolation(gridSize);
         while (!StdIn.isEmpty()) {
-            int origin = StdIn.readInt();
-            int destination = StdIn.readInt();
-            if (percolation.connected(origin, destination)) continue;
-            percolation.connect(origin, destination);
+            int xCod = StdIn.readInt();
+            int yCod = StdIn.readInt();
+            percolation.open(xCod, yCod);
+            //percolation.printArray();
             percolation.printGrid();
+            System.out.println(percolation.percolates());
         }
+
 	}
 }
---
+++ Perlocation/src/com/sc/percolation/again/PercolationStatsWithVisual.java
@@ -0,0 +1,82 @@
+package com.sc.percolation.again;
+
+import com.princeton.stdlib.StdDraw;
+import com.princeton.stdlib.StdIn;
+import com.princeton.stdlib.StdRandom;
+import com.princeton.stdlib.StdStats;
+
+public class PercolationStatsWithVisual {
+	private double[] percolationThresholds;
+	private int experiments;
+	
+	// perform T independent experiments on an N-by-N grid
+	public PercolationStatsWithVisual(int gridDimension, int experiments) {
+        if (gridDimension < 1) {
+            throw new IllegalArgumentException();
+        }
+        if (experiments < 1) {
+            throw new IllegalArgumentException();
+        }
+        percolationThresholds = new double[experiments];
+        this.experiments = experiments;
+        double totalCells = gridDimension * gridDimension;
+        //Perform the simulation experiments amout of time
+        for (int expIndex = 0; expIndex < experiments; expIndex++) {
+            Percolation percolation = new Percolation(gridDimension);
+            double openCells = 0;
+            double percolationThreshold = 0;
+            PercolationVisualizer.draw(percolation,gridDimension);
+            StdDraw.show(0);
+            do {
+                //Create a random position in the percolation Grid
+                int xOrdinate = StdIn.readInt();;
+                int yOrdinate = StdIn.readInt();;
+                if(!percolation.isOpen(xOrdinate, yOrdinate)){
+                    percolation.open(xOrdinate, yOrdinate);
+                    PercolationVisualizer.draw(percolation, gridDimension);
+                    StdDraw.show(10);
+                    openCells++;
+                }
+
+            }while (!percolation.percolates());
+            percolationThreshold = openCells / totalCells;
+            percolationThresholds[expIndex] = percolationThreshold;
+        }
+	}
+
+	// sample mean of percolation threshold
+	public double mean() {
+		return StdStats.mean(percolationThresholds);
+	}
+
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+		return StdStats.stddev(percolationThresholds);
+	}
+
+	// low endpoint of 95% confidence interval
+	public double confidenceLo() {
+		return (mean() - ((1.96 * stddev()) / Math.sqrt(experiments)));
+	}
+
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		return (mean() + ((1.96 * stddev()) / Math.sqrt(experiments)));
+	}
+
+	// test client (described below)
+	public static void main(String[] args) {
+		int gridSize = StdIn.readInt();;
+        int experiments = 1;
+        if ((gridSize <= 0) || (experiments <= 0)) {
+            throw new IllegalArgumentException("Illegal input");
+        }
+        PercolationStatsWithVisual percolationStats = new PercolationStatsWithVisual(gridSize,
+                experiments);
+        System.out.println("mean				=" + percolationStats.mean());
+        System.out.println("stddev				=" + percolationStats.stddev());
+        System.out.println("95% confidence interval		="
+                + percolationStats.confidenceLo() + ", "
+                + percolationStats.confidenceHi());
+	}
+}
